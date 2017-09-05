195d49ff5e8d24ad115da26aa90e15212989957f
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-09-05 10:38:14 +0100

    more debugging

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb://127.0.0.1/expressapi', (err) =>
+module.exports = mongoose.connect('mongodb://ryanknights:182493Lauren@127.0.0.1/expressapi', (err) =>
 {
 	if (err)
 	{
