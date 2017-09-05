885d95f0150fec864365a28594208ba1c7723dac
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-09-05 11:26:38 +0100

    more debugging

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb://127.0.0.1/expressapi', { user: 'ryanknights', pwd: '59NbrgDmp-L?bG!2'}, (err) =>
+module.exports = mongoose.connect('mongodb://127.0.0.1/expressapi??authSource=expressapi', { user: 'ryanknights', pwd: '59NbrgDmp-L?bG!2'}, (err) =>
 {
 	if (err)
 	{
