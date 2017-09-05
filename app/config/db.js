ced946fd0ced436b7287ae3178c775ae13bfa5bc
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-09-05 11:14:16 +0100

    more debugging

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb://ryanknights:59NbrgDmp-L?bG!2@127.0.0.1/expressapi', { uri_decode_auth: true } (err) =>
+module.exports = mongoose.connect('mongodb://ryanknights:59NbrgDmp-L?bG!2@127.0.0.1/expressapi', { uri_decode_auth: true }, (err) =>
 {
 	if (err)
 	{
