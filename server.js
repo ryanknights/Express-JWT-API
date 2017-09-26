b9a0d4072170eef1a5d321fc126fa213fab49497
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-09-26 14:12:07 +0100

    More debugging

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-app.options('/api', (req, res, next) =>
+app.options('/api/*', (req, res, next) =>
 {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
