47089194ce17da4d5f1732108dd2ef7ae5ad3686
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-09-26 14:05:44 +0100

    More debugging

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,22 +1,29 @@
 "use strict"
 
-const express     = require('express'),
-	  expressJwt  = require('express-jwt'),
-	  jwtSecret   = require('../config/secret'),
-	  usersCtrl   = require('../controllers/users');
+const express   = require('express'),
+	  expressJwt      = require('express-jwt'),
+	  jwtSecret       = require('../config/secret'),
+	  usersCtrl = require('../controllers/users');
 	
 module.exports = (() =>
 {
 	var api = express.Router();
 
-	api.use(expressJwt({secret : jwtSecret.secret}), (req, res, next) =>
+	api.use((req, res, next) =>
 	{
-		console.log(req.user);
-		if (!req.user || !req.user.isAdmin)
-		{
-			return res.send(403);
-		}
+		console.log('Middleware 1');
+		next();
 
+	}, (req, res, next) =>
+	{
+		console.log('Middleware 2');
+		console.log(req.user);
+		next();		
+	}, 
+	expressJwt({secret : jwtSecret.secret}), (req, res, next) =>
+	{
+		console.log('Middleware 3');
+		console.log(req.user);
 		next();
 	});
 
