97840ad82611ddc48c4c077bab1e7649b4c8d952
Ryan Knights <hello@ryanknights.co.uk>
2020-04-03 14:48:47 +0100

    fix: eslint errors for db connection

--- app/config/db.js
+++ app/config/db.js
@@ -4,24 +4,21 @@ mongoose.set('debug', process.env.DB_DEBUG);
 
 let connectionString = 'mongodb://';
 
-if (process.env.DB_AUTH === 'true')
-{
-	connectionString+= `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
+if (process.env.DB_AUTH === 'true') {
+  connectionString += `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
 }
 
-connectionString+= `${process.env.DB_HOST}/${process.env.DB_NAME}`;
+connectionString += `${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
 const config = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 };
-module.exports = mongoose.connect(connectionString, config, (err) =>
-{
-	if (err)
-	{
-		return console.log(err);
-	}
 
-	console.log('DB Connected');
+module.exports = mongoose.connect(connectionString, config, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log('DB Connected');
 });
